362469d2de3e25fb4cefcb428545d7fa4b5c0651
alexfanchina <alexfanchina@gmail.com>
2018-11-15 17:10:40 -0600

    implement milestone 3

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -9,6 +9,8 @@ namespace mxnet
 namespace op
 {
 
+#define TILE_WIDTH 32
+
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
 
@@ -23,6 +25,7 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     const int W_out = W - K + 1;
     (void)H_out; // silence declared but never referenced warning. remove this line when you start working
     (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+    const int W_grid = ceil(W_out / (TILE_WIDTH * 1.0));
 
 // An example use of these macros:
 // float a = y4d(0,0,0,0)
@@ -31,14 +34,32 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-    
+    int n, m, h, w, c, p, q;
+    n = blockIdx.x;
+    m = blockIdx.y;
+    h = blockIdx.z / W_grid * TILE_WIDTH + threadIdx.y;
+    w = blockIdx.z % W_grid * TILE_WIDTH + threadIdx.x;
+
+    if (n < B && m < M && h < H_out && w < W_out) {
+
+      float acc = 0;
+      for (c = 0; c < C; c++) {
+        for (p = 0; p < K; p++) {
+          for (q = 0; q < K; q++) {
+            acc += x4d(n, c, h + p, w + q) * k4d(m, c, p, q);
+          }
+        }
+      }
+      y4d(n, m, h, w) = acc;
+    }
+
 
 #undef y4d
 #undef x4d
 #undef k4d
 }
 
-/* 
+/*
    This function is called by new-inl.h
    Any code you write should be executed by this function.
    For ECE408, we only expect the float version of the operator to be called, so here we specialize with only floats.
@@ -49,24 +70,42 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
-    CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
+    // CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     // Extract the tensor dimensions into B,M,C,H,W,K
-    // ...
+    const int B = x.shape_[0];
+    const int M = y.shape_[1];
+    const int C = x.shape_[1];
+    const int H = x.shape_[2];
+    const int W = x.shape_[3];
+    const int K = w.shape_[3];
+    // const int mnist_dim = 28;
+
+    printf("B = %d\n", B);
+    printf("M = %d\n", M);
+    printf("C = %d\n", C);
+    printf("H = %d\n", H);
+    printf("W = %d\n", W);
+    printf("K = %d\n", K);
 
     // Set the kernel dimensions
-    // dim3 gridDim(0);
-    // dim3 blockDim(0);
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+    const int H_grid = ceil(H_out / (TILE_WIDTH * 1.0));
+    const int W_grid = ceil(W_out / (TILE_WIDTH * 1.0));
+    const int Z = H_grid * W_grid;
+    dim3 gridDim(B, M, Z);
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
 
     // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 
 }
 
-/* 
+/*
     This tells mxnet how to do an op when it's not a float.
     This is not used in the ECE408 project
 */
