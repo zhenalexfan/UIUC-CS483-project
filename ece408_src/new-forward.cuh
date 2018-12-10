7f77446fe6687baa474b371297a458fe676b1dbb
alexfanchina <alexfanchina@gmail.com>
2018-12-09 21:32:31 -0600

    fix (milestone 4)

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -113,6 +113,72 @@ namespace mxnet
 #undef w4d
     }
 
+    __global__ void forward_kernel_shared_memory_conv(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
+    {
+
+      /*
+         Modify this function to implement the forward pass described in Chapter 16.
+         We have added an additional dimension to the tensors to support an entire mini-batch
+         The goal here is to be correct AND fast.
+         We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
+       */
+
+      const int H_out = H - K + 1;
+      const int W_out = W - K + 1;
+      (void)H_out; // silence declared but never referenced warning. remove this line when you start working
+      (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+      const int W_grid = ceil(W_out / (TILE_WIDTH * 1.0));
+
+// An example use of these macros:
+// float a = y4d(0,0,0,0)
+// y4d(0,0,0,0) = a
+#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
+      int n, m, h, w, c, p, q;
+      n = blockIdx.x;
+      m = blockIdx.y;
+      h = blockIdx.z / W_grid * TILE_WIDTH + threadIdx.y;
+      w = blockIdx.z % W_grid * TILE_WIDTH + threadIdx.x;
+
+      __shared__ float x_s[12* (TILE_WIDTH + MASK_SIZE) * (TILE_WIDTH + MASK_SIZE)];
+
+#define xs3d(i2, i1, i0) x_s[(i2)*((TILE_WIDTH + MASK_SIZE) * (TILE_WIDTH + MASK_SIZE)) + (i1) * (TILE_WIDTH + MASK_SIZE) + i0]
+      for (c = 0; c < C; c++) {
+        xs3d(c, threadIdx.y, threadIdx.x) = x4d(n, c, h, w);
+        if (threadIdx.y < MASK_SIZE) {
+          xs3d(c, TILE_WIDTH + threadIdx.y, threadIdx.x) = x4d(n, c, TILE_WIDTH + h, w);
+        }
+        if (threadIdx.x < MASK_SIZE) {
+          xs3d(c, threadIdx.y, TILE_WIDTH + threadIdx.x) = x4d(n, c, h, TILE_WIDTH + w);
+        }
+        if (threadIdx.y < MASK_SIZE && threadIdx.x < MASK_SIZE) {
+          xs3d(c, TILE_WIDTH + threadIdx.y, TILE_WIDTH + threadIdx.x) = x4d(n, c, TILE_WIDTH + h, TILE_WIDTH + w);
+        }
+      }
+
+      __syncthreads();
+
+      if (n < B && m < M && h < H_out && w < W_out) {
+        float acc = 0;
+        for (c = 0; c < C; c++) {
+          for (p = 0; p < K; p++) {
+            for (q = 0; q < K; q++) {
+              // acc += x4d(n, c, h + p, w + q) * k4d(m, c, p, q);
+              acc += xs3d(c, threadIdx.y + p, threadIdx.x + q) * k4d(m, c, p, q);
+            }
+          }
+        }
+        y4d(n, m, h, w) = acc;
+      }
+
+
+#undef y4d
+#undef x4d
+#undef k4d
+    }
+
     __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
     {
 
@@ -160,7 +226,6 @@ namespace mxnet
 #undef x4d
 #undef k4d
     }
-
 /*
    This function is called by new-inl.h
    Any code you write should be executed by this function.
@@ -181,45 +246,55 @@ namespace mxnet
       const int H = x.shape_[2];
       const int W = x.shape_[3];
       const int K = w.shape_[3];
-      // const int mnist_dim = 28;
 
-      printf("B = %d\n", B);
-      printf("M = %d\n", M);
-      printf("C = %d\n", C);
-      printf("H = %d\n", H);
-      printf("W = %d\n", W);
-      printf("K = %d\n", K);
+      // printf("B = %d\n", B);
+      // printf("M = %d\n", M);
+      // printf("C = %d\n", C);
+      // printf("H = %d\n", H);
+      // printf("W = %d\n", W);
+      // printf("K = %d\n", K);
 
       // Set the kernel dimensions
       const int H_out = H - K + 1;
       const int W_out = W - K + 1;
-      //int W_unroll = H_out * W_out;
-      //int H_unroll = C * K * K;
       const int H_grid = ceil(H_out / (TILE_WIDTH * 1.0));
       const int W_grid = ceil(W_out / (TILE_WIDTH * 1.0));
       const int Z = H_grid * W_grid;
       dim3 gridDim(B, M, Z);
       dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
 
-      weightCopy = w.dptr_;
-      float hostWeight[14112];
-      cudaMemcpy(hostWeight, weightCopy, sizeof(float)*M*C*K*K, cudaMemcpyDeviceToHost);
-      cudaMemcpyToSymbol(weights, hostWeight, sizeof(float)*M*C*K*K, 0, cudaMemcpyHostToDevice);
+			/* Optimization 0: No Optimization */
+			/*forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K); */
 
-      /* float* X_unrolled;
-         cudaMalloc((void **) &X_unrolled, H_unroll * W_unroll * sizeof(float));
+      /* Optimization 1: Unroll/Shared-Memory Matrix Multiply */
+      /* int W_unroll = H_out * W_out;
+      int H_unroll = C * K * K;
 
-         for(int n = 0; n < B; n++){
-         unroll_kernel<<<ceil((float)(C*H_out*W_out/CUDA_MAX_NUM_THREADS)), CUDA_MAX_NUM_THREADS>>>(n, C, H, W, K, x.dptr_, X_unrolled);
-         cudaDeviceSynchronize();
-         matrixMultiplyShared<<<dim3(ceil((float)W_unroll/TILE_WIDTH), ceil((float)M/TILE_WIDTH), 1), dim3(TILE_WIDTH, TILE_WIDTH, 1)>>>(w.dptr_, X_unrolled, &y.dptr_[n*M*H_out*W_out], M, H_unroll, H_unroll, W_unroll, M, W_unroll);
-         cudaDeviceSynchronize();
-         }*/
+      float* X_unrolled;
+      cudaMalloc((void **) &X_unrolled, H_unroll * W_unroll * sizeof(float));
 
+      for(int n = 0; n < B; n++) {
+        unroll_kernel<<<ceil((float)(C*H_out*W_out/CUDA_MAX_NUM_THREADS)),
+          CUDA_MAX_NUM_THREADS>>>(
+          n, C, H, W, K, x.dptr_, X_unrolled);
+        cudaDeviceSynchronize();
+        matrixMultiplyShared<<<dim3(ceil((float)W_unroll/TILE_WIDTH), ceil((float)M/TILE_WIDTH), 1),
+          dim3(TILE_WIDTH, TILE_WIDTH, 1)>>>(
+          w.dptr_, X_unrolled, &y.dptr_[n*M*H_out*W_out], M,
+          H_unroll, H_unroll, W_unroll, M, W_unroll);
+        cudaDeviceSynchronize();
+      } */
 
-      // Call the kernel
-      //forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
-      forward_kernel_const<<<gridDim, blockDim>>>(y.dptr_,x.dptr_, B,M,C,H,W,K);
+      /* Optimization 2: Weight matrix (kernel values) in constant memory */
+      /* weightCopy = w.dptr_;
+         float hostWeight[14112];
+         cudaMemcpy(hostWeight, weightCopy, sizeof(float)*M*C*K*K, cudaMemcpyDeviceToHost);
+         cudaMemcpyToSymbol(weights, hostWeight, sizeof(float)*M*C*K*K, 0, cudaMemcpyHostToDevice);
+         forward_kernel_const<<<gridDim, blockDim>>>(y.dptr_,x.dptr_, B,M,C,H,W,K); */
+
+      /* Optimization 3: Shared Memory convolution */
+      forward_kernel_shared_memory_conv<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+
       // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
       MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
     }
